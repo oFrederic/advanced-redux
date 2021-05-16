2b7812c5898061d6ad679562a30ddb06aeb4fca4
Wojcikowski Frederic <wojcikowskif@gmail.com>
2021-05-16 14:48:37 +0900

    moving logic into an action creator thunk

--- src/store/cart.js
+++ src/store/cart.js
@@ -1,5 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+import { uiActions } from './ui';
+
 const initialState = {
   items: [],
   totalQuantity: 0,
@@ -43,6 +45,52 @@ const cart = createSlice({
     },
   },
 });
+
+export const sendCartData = cart => {
+  return async dispatch => {
+    dispatch(
+      uiActions.showNotification({
+        status: 'pending',
+        title: 'Sending...',
+        message: 'Sending cart data!',
+      })
+    );
+
+    const sendRequest = async () => {
+      const response = await fetch(
+        'https://redux-advanced-e71e4-default-rtdb.firebaseio.com/cart.json',
+        {
+          method: 'PUT',
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error('Sending cart data failed');
+      }
+    };
+
+    try {
+      await sendRequest();
+
+      dispatch(
+        uiActions.showNotification({
+          status: 'success',
+          title: 'Success...',
+          message: 'Sent cart successfully!',
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: 'error',
+          title: 'Error!',
+          message: 'Sending cart data failed!',
+        })
+      );
+    }
+  };
+};
 
 export const cartActions = cart.actions;
 export default cart.reducer;
